944bfe9437cbfbe3bd452d5b5ea3d94a6c0c4a98
zmartl <luca.marti@bluewin.ch>
2017-01-11 15:03:00 +0100

    js added

---
+++ EventmanagerStapoUst/js/category.js
@@ -0,0 +1,33 @@
+﻿var uri = 'api/category';
+
+$(document).ready(function () {
+    // Send an AJAX request
+    $.getJSON(uri)
+        .done(function (data) {
+            loadTable(data);
+        });
+});
+
+function loadTable(json) {
+    $.each(json, function (key, item) {
+        var table = document.getElementById('categoryTable');
+        var row = table.insertRow(0);
+        row.className = "clickable-row";
+        row.setAttribute("onclick", "document.location = 'detail.html?id=" + item.id + "'");
+
+        var category = row.insertCell(0);
+        category.innerHTML = item.name;
+    });
+}
+
+function find() {
+    var id = $('#categoryId').val();
+    $.getJSON(uri + '/' + id)
+        .done(function (data) {
+            console.log(data);
+            loadTable(data);
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('#category').text('Error: ' + err);
+        });
+}
---
+++ EventmanagerStapoUst/js/detail.js
@@ -0,0 +1,22 @@
+﻿var uri = 'api/event';
+
+$(document).ready(function () {
+    // Send an AJAX request
+
+    var id = $_GET("id");
+    if (!id) return;
+
+    $.getJSON(uri + "/" + id)
+        .done(function (data) {
+            loadTable(data);
+        });
+});
+
+function loadTable(item) {    
+    document.getElementById("eventName").value = item.eventName;
+    document.getElementById("eventLocation").value = item.eventLocation;
+    var cat = item.category;
+    document.getElementById("eventCategory").value = cat.name;
+    document.getElementById("eventStartTime").value = item.startDateTime;
+    document.getElementById("eventEndTime").value = item.endDateTime;
+}
